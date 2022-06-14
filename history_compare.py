701ad67a81ea2564c4d6d681db13dce26b2fd615
Siyu Chen <chensy96@gmail.com>
2022-06-14 11:50:25 +0200

    Add files via upload

---
+++ history_compare.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def compare(user_workflow, standard_workflow):
+
+    # print(standard_workflow['steps']['name'])
+
+    print("Every step was executed as expected, good job!")
