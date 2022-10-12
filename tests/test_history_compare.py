ea53fddd8daca9dd3f4fa6780c705404ec48e4f7
Siyu Chen <chensy96@gmail.com>
2022-10-12 22:27:41 +0200

    Update test_history_compare.py

--- tests/test_history_compare.py
+++ tests/test_history_compare.py
@@ -2,16 +2,41 @@ import unittest
 import sys
 from history_compare import *
 
-def add_fish_to_aquarium(fish_list):
-    if len(fish_list) > 10:
-        raise ValueError("A maximum of 10 fish can be added to the aquarium")
-    return {"tank_a": fish_list}
-
-
-class TestAddFishToAquarium(unittest.TestCase):
-    def test_add_fish_to_aquarium_success(self):
-        actual = add_fish_to_aquarium(fish_list=["shark", "tuna"])
-        expected = {"tank_a": ["shark", "tuna"]}
+class TestCompare(unittest.TestCase):
+    def test_compare(self):
+        std = {'a_galaxy_workflow': 'true', 'name': 'galaxy-101visible=true', 'steps': {'0': {'content_id': \
+    '__DATA_FETCH__', 'id': 0, 'input_connections': {},'inputs': [], 'name': 'Data Fetch', 'tool_id': '__DATA_FETCH__'}\
+    , '1': { 'content_id': '__DATA_FETCH__', 'id': 1, 'input_connections': {}, 'inputs': [], 'name': 'Data Fetch', \
+    'tool_id': '__DATA_FETCH__'}, '2': { 'content_id': \
+    'toolshed.g2.bx.psu.edu/repos/iuc/bedtools/bedtools_intersectbed/2.30.0', 'id': 2, 'input_connections': {'inputA':\
+    {'id': 1, 'output_name': 'output0'}, 'reduce_or_iterate|inputB': {'id': 1, 'output_name': 'output1'}}, 'inputs': []\
+    , 'label': None, 'name': 'bedtools Intersect intervals', 'outputs': [{'name': 'output', 'type': 'input'}], \
+    'tool_id': 'toolshed.g2.bx.psu.edu/repos/iuc/bedtools/bedtools_intersectbed/2.30.0', 'tool_state': \
+    '{"__input_ext": "input", "bed": "false", "header": "false", "split": "false", "strand": "", "__page__": null}',\
+    'tool_version': '2.30.0'}, '3': { 'content_id': 'toolshed.g2.bx.psu.edu/repos/iuc/datamash_ops/datamash_ops/1.1.0',\
+     'id': 3, 'input_connections': {'in_file': {'id': 2, 'output_name': 'output'}}, 'inputs': [], 'label': None, \
+     'name': 'Datamash', 'outputs': [{'name': 'out_file', 'type': 'tabular'}], \
+     'tool_id': 'toolshed.g2.bx.psu.edu/repos/iuc/datamash_ops/datamash_ops/1.1.0', 'tool_state': \
+     '{"__input_ext": "bed", "grouping": "4", "ignore_case": "false", "__rerun_remap_job_id__": null}', \
+     'tool_version':'1.1.0'}}}
+        usr = {'a_galaxy_workflow': 'true', 'name': 'Find exons with the highest number', 'steps': {'0': {'content_id': \
+    'None', 'id': 0, 'input_connections': {},'inputs': [], 'name': 'Input dataset', 'tool_id': 'None'}\
+    , '1': { 'content_id': 'None', 'id': 1, 'input_connections': {}, 'inputs': [], 'name': 'Input dataset', \
+    'tool_id': 'None'}, '2': { 'content_id': \
+    'toolshed.g2.bx.psu.edu/repos/iuc/bedtools/bedtools_intersectbed/2.30.0', 'id': 2, 'input_connections': {'inputA':\
+    {'id': 0, 'output_name': 'output'}, 'reduce_or_iterate|inputB': {'id': 1, 'output_name': 'output'}}, 'inputs': []\
+    , 'label': None, 'name': 'bedtools Intersect intervals', 'outputs': [{'name': 'output', 'type': 'input'}], \
+    'tool_id': 'toolshed.g2.bx.psu.edu/repos/iuc/bedtools/bedtools_intersectbed/2.30.0', 'tool_state': \
+    '{"__input_ext": "bed", "bed": "false", "header": "false", "split": "false", "strand": "", "__page__": null}',\
+    'tool_version': '2.30.0'}, '3': { 'content_id': 'toolshed.g2.bx.psu.edu/repos/iuc/datamash_ops/datamash_ops/1.1.0',\
+     'id': 3, 'input_connections': {'in_file': {'id': 2, 'output_name': 'output'}}, 'inputs': [], 'label': None, \
+     'name': 'Datamash', 'outputs': [{'name': 'out_file', 'type': 'tabular'}], \
+     'tool_id': 'toolshed.g2.bx.psu.edu/repos/iuc/datamash_ops/datamash_ops/1.1.0', 'tool_state': \
+     '{"__input_ext": "bed", "grouping": "4", "ignore_case": "false", "__rerun_remap_job_id__": null}', \
+     'tool_version':'1.1.0'}}}
+        temp = compare(std, usr)
+        actual = temp['number_of_steps']
+        expected = 2
         self.assertEqual(actual, expected)
 
 
