6c41fe403abad32e7a6fac6c85e0bdbdb9e1bb28
Siyu Chen <chensy96@gmail.com>
2022-10-12 21:58:52 +0200

    Update test_try.py

--- tests/test_try.py
+++ tests/test_try.py
@@ -1,5 +1,8 @@
 import unittest
-from  history_compare import *
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../')
+from  history_compare import test_get_input_id
 
 def add_fish_to_aquarium(fish_list):
     if len(fish_list) > 10:
