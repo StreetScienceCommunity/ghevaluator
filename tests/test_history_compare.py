70f96a847f3ce734cf002371ef7b9dd38b831033
Siyu Chen <chensy96@gmail.com>
2022-10-12 22:43:35 +0200

    Update test_history_compare.py

--- tests/test_history_compare.py
+++ tests/test_history_compare.py
@@ -79,3 +79,60 @@ class TestInptConnectionInitialize(unittest.TestCase):
         actual = inpt_connection_initialize()
         expected = {'expected_input_source': [], 'user_input_source': [], 'status': True}
         self.assertEqual(actual, expected)
+
+class TestSpliltId(unittest.TestCase):
+    def test_splilt_id(self):
+        actual = splilt_id("toolshed.g2.bx.psu.edu/repos/iuc/multiqc/multiqc/1.7")
+        expected = ('iuc', 'multiqc', '1.7')
+        self.assertEqual(actual, expected)
+
+class TestCountInputSteps(unittest.TestCase):
+    def test_count_input_steps(self):
+        std_case = {'0': {'id': 0, 'input_connections': {}, 'inputs': [{'description': '', 'name': 'Exons'}], 'label':\
+    'Exons', 'name': 'Input dataset', 'outputs': [], 'tool_id': None, 'tool_state': '{"optional": false}',\
+    'tool_version': None, 'type': 'data_input'}, '1': {'id': 1, 'input_connections': {}, 'inputs': [{'description': '',\
+    'name': 'Features'}], 'label': 'Features', 'name': 'Input dataset', 'outputs': [], 'tool_id': None, 'tool_state':\
+     '{"optional": false}', 'tool_version': None, 'type': 'data_input'}}
+        usr_case = {'0': {'id': 0, 'input_connections': {}, 'inputs': [{'description': '', 'name': 'Exons'}], 'label':\
+     'Exons', 'name': 'Data Fetch', 'outputs': [], 'tool_id': None, 'tool_state': '{"optional": false}', 'tool_version'\
+     : None, 'type': 'data_input'}, '1': {'id': 1, 'input_connections': {}, 'inputs': [{'description': '', 'name': \
+     'Features'}], 'label': 'Features', 'name': 'Data Fetch', 'outputs': [], 'tool_id': None, 'tool_state': \
+     '{"optional\": false}', 'tool_version': None, 'type': 'data_input'}}
+        actual = count_input_steps(std_case, usr_case)
+        expected = (2, 2)
+        self.assertEqual(actual, expected)
+
+class TestGetAllValues(unittest.TestCase):
+    def test_get_all_values(self):
+        std = {'0': {'content_id': \
+    '__DATA_FETCH__', 'id': 0, 'input_connections': {},'inputs': [], 'name': 'Data Fetch', 'tool_id': '__DATA_FETCH__'}\
+    , '1': { 'content_id': '__DATA_FETCH__', 'id': 1, 'input_connections': {}, 'inputs': [], 'name': 'Data Fetch', \
+    'tool_id': '__DATA_FETCH__'}, '2': { 'content_id': \
+    'toolshed.g2.bx.psu.edu/repos/iuc/bedtools/bedtools_intersectbed/2.30.0', 'id': 2, 'input_connections': {'inputA':\
+    {'id': 1, 'output_name': 'output0'}, 'reduce_or_iterate|inputB': {'id': 1, 'output_name': 'output1'}}, 'inputs': []\
+    , 'label': None, 'name': 'bedtools Intersect intervals', 'outputs': [{'name': 'output', 'type': 'input'}], \
+    'tool_id': 'toolshed.g2.bx.psu.edu/repos/iuc/bedtools/bedtools_intersectbed/2.30.0', 'tool_state': \
+    '{"__input_ext": "input", "bed": "false", "header": "false", "split": "false", "strand": "", "__page__": null}',\
+    'tool_version': '2.30.0'}, '3': { 'content_id': 'toolshed.g2.bx.psu.edu/repos/iuc/datamash_ops/datamash_ops/1.1.0',\
+     'id': 3, 'input_connections': {'in_file': {'id': 2, 'output_name': 'output'}}, 'inputs': [], 'label': None, \
+     'name': 'Datamash', 'outputs': [{'name': 'out_file', 'type': 'tabular'}], \
+     'tool_id': 'toolshed.g2.bx.psu.edu/repos/iuc/datamash_ops/datamash_ops/1.1.0', 'tool_state': \
+     '{"__input_ext": "bed", "grouping": "4", "ignore_case": "false", "__rerun_remap_job_id__": null}', \
+     'tool_version':'1.1.0'}}
+        usr = {'0': {'content_id': \
+    'None', 'id': 0, 'input_connections': {},'inputs': [], 'name': 'Input dataset', 'tool_id': 'None'}\
+    , '1': { 'content_id': 'None', 'id': 1, 'input_connections': {}, 'inputs': [], 'name': 'Input dataset', \
+    'tool_id': 'None'}, '2': { 'content_id':\
+    'toolshed.g2.bx.psu.edu/repos/iuc/bedtools/bedtools_intersectbed/2.30.0', 'id': 2, 'input_connections': {'inputA':\
+    {'id': 0, 'output_name': 'output'}, 'reduce_or_iterate|inputB': {'id': 1, 'output_name': 'output'}}, 'inputs': []\
+    , 'label': None, 'name': 'bedtools Intersect intervals', 'outputs': [{'name': 'output', 'type': 'input'}], \
+    'tool_id': 'toolshed.g2.bx.psu.edu/repos/iuc/bedtools/bedtools_intersectbed/2.30.0', 'tool_state': \
+    '{"__input_ext": "bed", "bed": "false", "header": "false", "split": "false", "strand": "", "__page__": null}',\
+    'tool_version': '2.30.0'}, '3': { 'content_id': 'toolshed.g2.bx.psu.edu/repos/iuc/datamash_ops/datamash_ops/1.1.0',\
+     'id': 3, 'input_connections': {'in_file': {'id': 2, 'output_name': 'output'}}, 'inputs': [], 'label': None, \
+     'name': 'Datamash', 'outputs': [{'name': 'out_file', 'type': 'tabular'}], \
+     'tool_id': 'toolshed.g2.bx.psu.edu/repos/iuc/datamash_ops/datamash_ops/1.1.0', 'tool_state': \
+     '{"__input_ext": "bed", "grouping": "4", "ignore_case": "false", "__rerun_remap_job_id__": null}', \
+     'tool_version':'1.1.0'}}
+        temp = get_all_values(std, usr)
+        self.assertTrue(temp['steps'][0]['tool_used']['expected_value'] == 'bedtools Intersect intervals')
