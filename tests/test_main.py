b3bac48511515d650c61851dd3c1dec6cf4692b5
Siyu Chen <chensy96@gmail.com>
2022-10-19 23:28:34 +0200

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,12 +7,6 @@ class TestGetHistory(unittest.TestCase):
         expected = ('96db5bbbc9a86365', 'galaxy-101')
         self.assertEqual(expected, actual)
 
-class TestGenerateReportFile(unittest.TestCase):
-    def test_generate_report_file(self):
-        temp = generate_report_file("wrong path", "data")
-        actual = temp
-        expected = "wrong path"
-        self.assertEqual(expected, actual)
 
 class TestGetUserWorkflow(unittest.TestCase):
     def test_get_user_workflow(self):
