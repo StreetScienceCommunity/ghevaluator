67ca5ecbf679a7c048bb84ee69d4f536ae9e9e0d
Siyu Chen <chensy96@gmail.com>
2022-10-25 22:14:43 +0200

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,17 +3,17 @@ import os.path
 import sys
 tdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..\ghevaluator'))
 sys.path.insert(1, tdir)
-from main import *
+import main
 
 class TestGetHistory(unittest.TestCase):
     def test_get_history(self):
-        actual = get_history("https://usegalaxy.eu/u/berenice/h/galaxy-101")
+        actual = main.get_history("https://usegalaxy.eu/u/berenice/h/galaxy-101")
         expected = ('96db5bbbc9a86365', 'galaxy-101')
         self.assertEqual(expected, actual)
 
 
 class TestGetUserWorkflow(unittest.TestCase):
     def test_get_user_workflow(self):
-        actual = get_user_workflow("96db5bbbc9a86365", "galaxy-101", "D4XEpojvk877VKOAtCpu8H2Irdr3kol")
+        actual = main.get_user_workflow("96db5bbbc9a86365", "galaxy-101", "D4XEpojvk877VKOAtCpu8H2Irdr3kol")
         expected = "a_galaxy_workflow"
         self.assertIn(expected, actual)
