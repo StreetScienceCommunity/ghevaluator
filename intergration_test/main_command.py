c096b525ebad0f0cc9763835dcc6f83ef17154fa
Siyu Chen <chensy96@gmail.com>
2022-10-25 23:39:24 +0200

    Update main_command.py

--- intergration_test/main_command.py
+++ intergration_test/main_command.py
@@ -10,6 +10,6 @@ subprocess.check_call(
      'https://usegalaxy.eu/u/siyu_chen/h/assemblyhands-onsiyu-chen',
      'https://usegalaxy.eu/training-material/topics/assembly/tutorials/general-introduction/workflows/assembly-general-introduction.ga',
      'D4XEpojvk877VKOAtCpu8H2Irdr3kol'])
-assert os.path.exists('./reporbt.json')
+assert os.path.exists('./report.json')
 
 
