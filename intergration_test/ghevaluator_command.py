65df72571da822f7cdcfdb5d3745a9456ed282f2
Siyu Chen <chensy96@gmail.com>
2022-10-26 23:51:29 +0200

    Update ghevaluator_command.py

--- intergration_test/ghevaluator_command.py
+++ intergration_test/ghevaluator_command.py
@@ -2,7 +2,7 @@ import subprocess
 import os.path
 
 subprocess.check_call(
-    ['python', 'ghevaluator/ghevaluator_main.py',
+    ['python', '../ghevaluator/ghevaluator_main.py',
      'https://usegalaxy.eu/u/siyu_chen/h/assemblyhands-onsiyu-chen',
      'https://usegalaxy.eu/training-material/topics/assembly/tutorials/general-introduction/workflows/assembly-general-introduction.ga',
      'D4XEpojvk877VKOAtCpu8H2Irdr3kol'])
