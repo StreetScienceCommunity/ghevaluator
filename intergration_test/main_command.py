142859808628a233369ac0dc01dc77d4f9d4876f
Siyu Chen <chensy96@gmail.com>
2022-10-26 20:59:22 +0200

    Update main_command.py

--- intergration_test/main_command.py
+++ intergration_test/main_command.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 import os.path
 import sys
@@ -6,7 +5,7 @@ tdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../ghevaluator')
 sys.path.insert(1, tdir)
 
 subprocess.check_call(
-    ['python', '../ghevaluator/main.py',
+    ['python', 'ghevaluator/main.py',
      'https://usegalaxy.eu/u/siyu_chen/h/assemblyhands-onsiyu-chen',
      'https://usegalaxy.eu/training-material/topics/assembly/tutorials/general-introduction/workflows/assembly-general-introduction.ga',
      'D4XEpojvk877VKOAtCpu8H2Irdr3kol'])
