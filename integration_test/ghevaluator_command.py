c8d85898f1fcbf0bd17deba90705d053b6332847
Siyu Chen <chensy96@gmail.com>
2022-11-01 22:49:37 +0100

    Update ghevaluator_command.py

--- integration_test/ghevaluator_command.py
+++ integration_test/ghevaluator_command.py
@@ -1,9 +1,25 @@
 import subprocess
 import os.path
+import json
 
+# check the program with a perfect history following beerdecoded tutorial
 subprocess.check_call(
     ['python', 'ghevaluator/ghevaluator_main.py',
-     'https://usegalaxy.eu/u/siyu_chen/h/assemblyhands-onsiyu-chen',
-     'https://usegalaxy.eu/training-material/topics/assembly/tutorials/general-introduction/workflows/assembly-general-introduction.ga',
+     'https://usegalaxy.eu/u/siyu_chen/h/beerdecoded',
+     'https://training.galaxyproject.org/training-material/topics/metagenomics/tutorials/beer-data-analysis/workflows/main_workflow.ga',
      'D4XEpojvk877VKOAtCpu8H2Irdr3kol'])
-assert os.path.exists('./report.json')
+with open(os.path.join(os.path.dirname(__file__), 'report.json'), 'r') as f:
+    report_str = f.read()
+    actual = json.loads(report_str)
+assert actual['number_of_wrong_steps'] == 0
+
+# check the program with a false history following beerdecoded tutorial
+subprocess.check_call(
+    ['python', 'ghevaluator/ghevaluator_main.py',
+     'https://usegalaxy.eu/u/siyu_chen/h/blankhisotrytry',
+     'https://training.galaxyproject.org/training-material/topics/metagenomics/tutorials/beer-data-analysis/workflows/main_workflow.ga',
+     'D4XEpojvk877VKOAtCpu8H2Irdr3kol'])
+with open(os.path.join(os.path.dirname(__file__), 'report.json'), 'r') as f:
+    report_str = f.read()
+    actual = json.loads(report_str)
+assert actual['number_of_wrong_steps'] == 6
