520b5b89fd4635b3d22288a8b1d69329d004ac52
Siyu Chen <chensy96@gmail.com>
2022-11-01 23:00:22 +0100

    Update ghevaluator_command.py

--- integration_test/ghevaluator_command.py
+++ integration_test/ghevaluator_command.py
@@ -8,9 +8,9 @@ subprocess.check_call(
      'https://usegalaxy.eu/u/siyu_chen/h/beerdecoded',
      'https://training.galaxyproject.org/training-material/topics/metagenomics/tutorials/beer-data-analysis/workflows/main_workflow.ga',
      'D4XEpojvk877VKOAtCpu8H2Irdr3kol'])
-with open(os.path.join(os.path.dirname(__file__), 'report.json'), 'r') as f:
-    report_str = f.read()
-    actual = json.loads(report_str)
+f = open('report.json', 'r')
+report_str = f.read()
+actual = json.loads(report_str)
 assert actual['number_of_wrong_steps'] == 0
 
 # check the program with a false history following beerdecoded tutorial
@@ -19,7 +19,7 @@ subprocess.check_call(
      'https://usegalaxy.eu/u/siyu_chen/h/blankhisotrytry',
      'https://training.galaxyproject.org/training-material/topics/metagenomics/tutorials/beer-data-analysis/workflows/main_workflow.ga',
      'D4XEpojvk877VKOAtCpu8H2Irdr3kol'])
-with open(os.path.join(os.path.dirname(__file__), 'report.json'), 'r') as f:
-    report_str = f.read()
-    actual = json.loads(report_str)
+f = open('report.json', 'r')
+report_str = f.read()
+actual = json.loads(report_str)
 assert actual['number_of_wrong_steps'] == 6
